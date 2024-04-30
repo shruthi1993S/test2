11771b899f9728b0f181b71879ddad58ecf078f1
shruthi1993S <163557058+shruthi1993S@users.noreply.github.com>
2024-04-30 22:26:14 +0530

    Add files via upload
    n

---
+++ countdown.js
@@ -0,0 +1,32 @@
+
+var counterDownElement=document.getElementById("countdown");
+setTimeout(function() {
+    counterDownElement.textContent="9";
+    setTimeout(function() {
+        counterDownElement.textContent="8";
+        setTimeout(function() {
+            counterDownElement.textContent="7";
+            setTimeout(function() {
+                counterDownElement.textContent="6";
+                setTimeout(function() {
+                    counterDownElement.textContent="5";
+                    setTimeout(function() {
+                        counterDownElement.textContent="4";
+                        setTimeout(function() {
+                            counterDownElement.textContent="3";
+                            setTimeout(function() {
+                                counterDownElement.textContent="2";
+                                setTimeout(function() {
+                                    counterDownElement.textContent="1";
+                                    setTimeout(function() {
+                                        counterDownElement.textContent="Happy NewYear !!!";
+},1000 );
+},1000 );
+},1000 );
+},1000 );
+},1000 );
+},1000 );
+},1000 );
+},1000 );
+},1000 );
+},1000 );
